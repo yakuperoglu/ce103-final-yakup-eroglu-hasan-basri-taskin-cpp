6ae263c49c62de6977d7069658bf58ec101f1efc
yakup <yakup_eroglu22@erdogan.edu.tr>
2023-12-15 15:18:47 +0300

    Arrangements required to start the project clean

--- src/librarysystem/header/librarysystem.h
+++ src/librarysystem/header/librarysystem.h
@@ -1,9 +1,3 @@
-/**
- * @file librarysystem.h
- *
- * @brief Provides functions for math. utilities
- */
-
 #ifndef LIBRARYSYSTEM_H
 #define LIBRARYSYSTEM_H
 
@@ -11,49 +5,15 @@
 
 namespace Coruh {
 namespace Librarysystem {
-/**
-    @class Librarysystem
-    @brief Provides Basic functions for various operations.
-*/
+
 class Librarysystem {
  public:
-  /**
-   * Adds two numbers.
-   * @param a First operand.
-   * @param b Second operand.
-   * @return The sum of a and b.
-   */
-  static double add(double a, double b);
 
-  /**
-   * Subtracts the second number from the first.
-   * @param a Minuend.
-   * @param b Subtrahend.
-   * @return The result of a - b.
-   */
-  static double subtract(double a, double b);
 
-  /**
-   * Multiplies two numbers.
-   * @param a First operand.
-   * @param b Second operand.
-   * @return The product of a and b.
-   */
-  static double multiply(double a, double b);
-
-  /**
-   * Divides the first number by the second.
-   * Throws std::invalid_argument if the second number is zero.
-   * @param a Dividend.
-   * @param b Divisor.
-   * @return The result of a / b.
-   * @throws std::invalid_argument If b is zero.
-   */
-  static double divide(double a, double b);
 
 
 };
 }
 }
 
-#endif // LIBRARYSYSTEM_H
+#endif
--- src/librarysystem/src/librarysystem.cpp
+++ src/librarysystem/src/librarysystem.cpp
@@ -2,23 +2,4 @@
 #include <stdexcept>
 
 using namespace Coruh::Librarysystem;
-
-double Librarysystem::add(double a, double b) {
-  return a + b;
-}
-
-double Librarysystem::subtract(double a, double b) {
-  return a - b;
-}
-
-double Librarysystem::multiply(double a, double b) {
-  return a * b;
-}
-
-double Librarysystem::divide(double a, double b) {
-  if (b == 0) {
-    throw std::invalid_argument("Division by zero is not allowed.");
-  }
-
-  return a / b;
-}
+using namespace std;
