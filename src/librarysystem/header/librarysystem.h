9b3d49cb6cffc32b1165c4d6bdd08eb706f8c501
HBasriTaskin <taskin.hsnbasri@gmail.com>
2023-12-17 13:42:51 +0300

    Basic structure established

--- src/librarysystem/header/librarysystem.h
+++ src/librarysystem/header/librarysystem.h
@@ -1,14 +1,39 @@
 #ifndef LIBRARYSYSTEM_H
 #define LIBRARYSYSTEM_H
 
-#include "../../utility/header/commonTypes.h"
+#include <string>
 
+#include "../../utility/header/commonTypes.h"
+using namespace std;
 namespace Coruh {
 namespace Librarysystem {
+struct User {
+  string email;
+  string name;
+  string surname;
 
-class Librarysystem {
- public:
 };
+struct UserAuthentication {
+  User login(const string &email, const string &password);
+  bool registerUser(const string &email, const string &name, const string &surname, const string &password);
+
+};
+
+struct BookSystem {
+
+};
+struct LoanManagment {
+
+};
+
+struct WishList {
+
+};
+
+struct ReadingTracker {
+
+};
+
 }
 }
 
