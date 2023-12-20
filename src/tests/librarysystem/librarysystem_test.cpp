5158ae8b56576deda84e7a442ff7ee3b7cc22b86
yakup <yakup_eroglu22@erdogan.edu.tr>
2023-12-20 15:28:17 +0300

    a little bit changes

--- src/tests/librarysystem/librarysystem_test.cpp
+++ src/tests/librarysystem/librarysystem_test.cpp
@@ -1,15 +1,42 @@
-#include "gtest/gtest.h"
-#include "../../librarysystem/header/librarysystem.h"
+#include "LibrarySystem.h"
+#include <gtest/gtest.h>
 
-class LibrarysystemTest : public ::testing::Test {
+class UserAuthenticationTest : public ::testing::Test {
  protected:
-  void SetUp() override {
-  }
-
-  void TearDown() override {
-  }
+  UserAuthentication auth;
 };
 
+TEST_F(UserAuthenticationTest, LoginWithValidCredentials) {
+  // Örnek geçerli kimlik bilgileri
+  std::string email = "test@example.com";
+  std::string password = "password123";
+  // Geçerli kimlik bilgileri ile giriþ yapma testi
+  User user = auth.login(email, password);
+  // Beklenen sonucun kontrolü
+  EXPECT_EQ(user.email, email);
+}
+
+TEST_F(UserAuthenticationTest, LoginWithInvalidCredentials) {
+  // Örnek geçersiz kimlik bilgileri
+  std::string email = "wrong@example.com";
+  std::string password = "wrongpassword";
+  // Geçersiz kimlik bilgileri ile giriþ yapma testi
+  User user = auth.login(email, password);
+  // Beklenen sonucun kontrolü
+  EXPECT_EQ(user.email, "");
+}
+
+TEST_F(UserAuthenticationTest, RegisterUser) {
+  // Yeni kullanýcý kaydý için bilgiler
+  std::string email = "newuser@example.com";
+  std::string name = "New";
+  std::string surname = "User";
+  std::string password = "newpassword";
+  // Kullanýcý kaydý testi
+  bool result = auth.registerUser(email, name, surname, password);
+  // Beklenen sonucun kontrolü
+  EXPECT_TRUE(result);
+}
 
 
 int main(int argc, char **argv) {
