ee329b50c3f77d8884aaa8f7cad03ea49e96548c
yakup <yakup_eroglu22@erdogan.edu.tr>
2023-12-30 13:16:44 +0300

    big changes big step

--- src/librarysystem/src/librarysystem.cpp
+++ src/librarysystem/src/librarysystem.cpp
@@ -14,22 +14,49 @@ void clearScreen() {
 }
 
 bool BookSystem::addBook() {
-  cout << "hello ";
+  cout << "hello";
   return 0;
 }
 
 bool BookSystem::deleteBook() {
-  cout << "hello ";
+  cout << "hello";
   return 0;
 }
 
 bool BookSystem::updateBook() {
-  cout << "hello ";
+  cout << "hello";
   return 0;
 }
 
+bool BookSystem::viewCatalog() {
+  cout << "hello";
+  return 0;
+}
 
-bool userOperations() {
+/*
+User UserAuthentication::login(const char* email, const char* password) {
+  const char* expectedEmail = "user@example.com";
+  const char* expectedPassword = "12345";
+
+  if (strcmp(email, expectedEmail) == 0 && strcmp(password, expectedPassword) == 0) {
+    return ;
+  }
+  return ;
+}
+
+
+bool UserAuthentication::registerUser(const char* email, const char* name, const char* surname, const char* password) {
+
+  if (email != nullptr && name != nullptr && surname != nullptr && password != nullptr) {
+
+    return true;
+  }
+  return false;
+}
+*/
+// dear hasan I couldnt do this part so ý just send to you :))
+
+bool operationsFunc::bookCataLoging() {
   int choice;
 
   while (true) {
@@ -38,8 +65,16 @@ bool userOperations() {
     cout << "1. Add Book\n";
     cout << "2. Delete Book\n";
     cout << "3. Update Book\n";
-    cout << "4. Return Main Menu\n";
+    cout << "4. View Catalog\n";
+    cout << "5. Return Main Menu\n";
     cin >> choice;
+
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Invalid input. Please enter a number.\n";
+      continue;
+    }
 
     switch (choice) {
       case 1:
@@ -58,6 +93,10 @@ bool userOperations() {
         break;
 
       case 4:
+        BookSystem::viewCatalog();
+        break;
+
+      case 5:
         return 0;
         break;
 
@@ -69,68 +108,160 @@ bool userOperations() {
   return 0;
 }
 
-bool adminOperations() {
-  int choice;
-  cin >> choice;
-
-  while (true) {
-    switch (choice) {
-      case 1:
-        return 0;
-        break;
-
-      default:
-        break;
-    }
-  }
+bool operationsFunc::loanManagement() {
   return 0;
 }
 
-bool guestOperations() {
-  int choice;
-  cin >> choice;
-
-  while (true) {
-    switch (choice) {
-      case 1:
-        return 0;
-        break;
-
-      default:
-        break;
-    }
-  }
-
+bool operationsFunc::wishList() {
   return 0;
 }
+
+bool operationsFunc::readingTracker() {
+  return 0;
+}
+
+bool WishList::wishList() {
+  return 0;
+}
+
+bool WishList::removeFromWishList() {
+  return 0;
+}
+
+bool userOperations() {
   int choice;
 
   while (true) {
     clearScreen();
-    cout << "Welcome To Personal Library System\nPlease Login\n\n";
-    cout << "1. User Operations\n";
-    cout << "2. Admin Operations\n";
-    cout << "3. Guest Opreations\n";
+    cout << "welcome to User Operations\n\n";
+    cout << "1. Book Cataloging\n";
+    cout << "2. Loan Management\n";
+    cout << "3. WishList Management\n";
+    cout << "4. Reading Tracker\n";
+    cout << "5. Return Main Menu\n";
+    cin >> choice;
+
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Invalid input. Please enter a number.\n";
+      continue;
+    }
+
+    switch (choice) {
+      case 1:
+        operationsFunc::bookCataLoging();
+        return 0;
+        break;
+
+      case 2:
+        operationsFunc::loanManagement();
+        return 0;
+        break;
+
+      case 3:
+        operationsFunc::wishList();
+        return 0;
+        break;
+
+      case 4:
+        operationsFunc::readingTracker();
+        return 0;
+        break;
+
+      case 5:
+        return 0;
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  return 0;
+}
+
+bool guestOperation() {
+  int choice;
+
+  while (true) {
+    cout << "\n";
+    cout << "1. View Catalog\n";
+    cout << "2. Return MainMenu\n";
+    cin >> choice;
+
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Invalid input. Please enter a number.\n";
+      clearScreen();
+      continue;
+    }
+
+    switch (choice) {
+      case 1:
+        int choice2;
+        clearScreen();
+        BookSystem::viewCatalog();
+        cout << "\n\n1 To Return MainMenu";
+        cin >> choice2;
+
+        switch (choice2) {
+          case 1:
+            return mainMenu;
+
+          default:
+            break;
+        }
+
+        break;
+
+      case 2:
+        return 0;
+
+      default:
+        cout << "Invalid choice. Please try again.\n";
+        break;
+    }
+  }
+
+  return 0;
+}//guests can only view the catalog
+
+int mainMenu() {
+  int choice;
+
+  while (true) {
+    clearScreen();
+    cout << "Welcome To Personal Library System\n\n";
+    cout << "1. Login\n";
+    cout << "2. Register\n";
+    cout << "3. Guest Mode\n";
     cout << "4. Exit Program\n";
     cin >> choice;
+
+    if (cin.fail()) {
+      cin.clear();
+      cin.ignore(numeric_limits<streamsize>::max(), '\n');
+      cout << "Invalid input. Please enter a number.\n";
+      continue;
+    }
 
     switch (choice) {
       case 1:
         clearScreen();
-        cout << "User Operations\n";
-        userOperations();
+        // login func
         break;
 
       case 2:
         clearScreen();
-        cout << "Admin Operations\n";
-        adminOperations();
+        // register func
         break;
 
       case 3:
         clearScreen();
         cout << "Guest Operations\n";
-        guestOperations();
+        guestOperation();
         break;
 
       case 4:
--- src/tests/librarysystem/librarysystem_test.cpp
+++ src/tests/librarysystem/librarysystem_test.cpp
@@ -1,6 +1,7 @@
 #include "LibrarySystem.h"
 #include <gtest/gtest.h>
 
+using namespace std;
 class UserAuthenticationTest : public ::testing::Test {
  protected:
   UserAuthentication auth;
@@ -8,18 +9,18 @@ class UserAuthenticationTest : public ::testing::Test {
 
 TEST_F(UserAuthenticationTest, LoginWithValidCredentials) {
   // Örnek geçerli kimlik bilgileri
-  std::string email = "test@example.com";
-  std::string password = "password123";
+  char email[50] = "test@example.com";
+  char password[50] = "password123";
   // Geçerli kimlik bilgileri ile giriþ yapma testi
-  User user = auth.login(email, password);
+  User user = auth.login(email *, password);
   // Beklenen sonucun kontrolü
   EXPECT_EQ(user.email, email);
 }
 
 TEST_F(UserAuthenticationTest, LoginWithInvalidCredentials) {
   // Örnek geçersiz kimlik bilgileri
-  std::string email = "wrong@example.com";
-  std::string password = "wrongpassword";
+  string email = "wrong@example.com";
+  string password = "wrongpassword";
   // Geçersiz kimlik bilgileri ile giriþ yapma testi
   User user = auth.login(email, password);
   // Beklenen sonucun kontrolü
@@ -28,10 +29,10 @@ TEST_F(UserAuthenticationTest, LoginWithInvalidCredentials) {
 
 TEST_F(UserAuthenticationTest, RegisterUser) {
   // Yeni kullanýcý kaydý için bilgiler
-  std::string email = "newuser@example.com";
-  std::string name = "New";
-  std::string surname = "User";
-  std::string password = "newpassword";
+  string email = "newuser@example.com";
+  string name = "New";
+  string surname = "User";
+  string password = "newpassword";
   // Kullanýcý kaydý testi
   bool result = auth.registerUser(email, name, surname, password);
   // Beklenen sonucun kontrolü
