96464574a7aa56ce75631db1f98af727703478e6
HBasriTaskin <taskin.hsnbasri@gmail.com>
2023-12-30 14:41:39 +0300

    Some problems happened because of couldnt resolve conflict's

--- src/librarysystem/header/librarysystem.h
+++ src/librarysystem/header/librarysystem.h
@@ -16,6 +16,7 @@ struct User {
 struct UserAuthentication {
   User login(const char *email, const char *password);
   bool registerUser(const char *email, const char *name, const char *surname, const char *password);
+  static bool writeUser(const User &user);
 };
 
 struct operationsFunc {
